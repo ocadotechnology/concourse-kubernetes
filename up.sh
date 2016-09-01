c2ecfbc5279b3cf135859df3aa87f85ee7e3caca
Mike Bryant <m@ocado.com>
2016-09-01 15:32:47 +0100

    Use Deployment instead of RC

--- up.sh
+++ up.sh
@@ -3,9 +3,9 @@ set -e
 
 kubectl create -f manifests/concourse-postgresql-secrets.yaml
 kubectl create -f manifests/concourse-postgresql-svc.yaml
-kubectl create -f manifests/concourse-postgresql-rc.yaml
+kubectl create -f manifests/concourse-postgresql-deployment.yaml
 
 kubectl create -f manifests/concourse-secrets.yaml
 kubectl create -f manifests/concourse-web-svc.yaml
-kubectl create -f manifests/concourse-web-rc.yaml
-kubectl create -f manifests/concourse-worker-rc.yaml
+kubectl create -f manifests/concourse-web-deployment.yaml
+kubectl create -f manifests/concourse-worker-deployment.yaml
